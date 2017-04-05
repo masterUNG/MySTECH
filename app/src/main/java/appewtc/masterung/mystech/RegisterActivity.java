64f63921000f4dc5120305072c4f1d9daf335108
masterUNG <phrombutr@gmail.com>
2017-04-05 15:01:17 +0700

    Test

--- app/src/main/java/appewtc/masterung/mystech/RegisterActivity.java
+++ app/src/main/java/appewtc/masterung/mystech/RegisterActivity.java
@@ -1,13 +1,47 @@
 package appewtc.masterung.mystech;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText nameEditText, surnameEditText, addressEditText,
+            userEditText, passwordEditText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initialView();
+
+        controller();
+
+    }   // Main Method
+
+    private void controller() {
+        button.setOnClickListener(RegisterActivity.this);
     }
-}
+
+    private void initialView() {
+        nameEditText = (EditText) findViewById(R.id.edtName);
+        surnameEditText = (EditText) findViewById(R.id.edtSurname);
+        addressEditText = (EditText) findViewById(R.id.edtAddress);
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        button = (Button) findViewById(R.id.btnSave);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        //Get Value From EditText
+
+
+    }   // onClick
+
+}   // Main Class
