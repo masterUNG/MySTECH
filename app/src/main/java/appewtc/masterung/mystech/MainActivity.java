6440074f35913e804e990622ab61d4e3a8c5ada3
masterUNG <phrombutr@gmail.com>
2017-04-05 10:26:07 +0700

    Start 5April

--- app/src/main/java/appewtc/masterung/mystech/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mystech/MainActivity.java
@@ -1,13 +1,58 @@
 package appewtc.masterung.mystech;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initial View คือการผูกตัวแปร กับ View ที่ อยู่ที่ Activity
+        initialView();
+
+        //Create Controller
+        controller();
+
+
+    }   // Main Method
+
+    private void controller() {
+        textView.setOnClickListener(MainActivity.this);
+        button.setOnClickListener(MainActivity.this);
     }
-}
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        String tag = "SriwattanaV1";
+
+        //For TextView
+        if (view == textView) {
+            Log.d(tag, "You Click TextView");
+        }
+
+
+    }   // onClick
+
+}   // Main Class นี่คือ คลาสหลัก
