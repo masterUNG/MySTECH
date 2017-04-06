30f244c5b2475374e43dafb96207f95618d2df7e
masterUNG <phrombutr@gmail.com>
2017-04-06 16:09:09 +0700

    Project Success

--- app/src/main/java/appewtc/masterung/mystech/ServiceActivity.java
+++ app/src/main/java/appewtc/masterung/mystech/ServiceActivity.java
@@ -2,6 +2,12 @@ package appewtc.masterung.mystech;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ServiceActivity extends AppCompatActivity {
 
@@ -9,5 +15,36 @@ public class ServiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
-    }
-}
+
+        ListView listView = (ListView) findViewById(R.id.livName);
+
+        try {
+
+            GetUser getUser = new GetUser(ServiceActivity.this);
+            getUser.execute();
+
+            String strJSON = getUser.get();
+            Log.d("TestV3", "JSON ==> " + strJSON);
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+
+            String[] nameStrings = new String[jsonArray.length()];
+            for (int i=0;i<jsonArray.length();i++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                nameStrings[i] = jsonObject.getString("Name");
+
+            }   // for
+
+            //Create ListView
+            ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(ServiceActivity.this,
+                    android.R.layout.simple_list_item_1, nameStrings);
+            listView.setAdapter(stringArrayAdapter);
+
+        } catch (Exception e) {
+            Log.d("TestV3", "e Main ==> " + e.toString());
+        }
+
+    }       // Main Method
+
+}   // Main Class
